4b74b95eacae4f55a131190c025ad51bfce65fa1
NickOttrando <nottrando@gmail.com>
2014-04-24 14:58:03 -0700

    wrote queen piece

---
+++ src/chessgame/Queen.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package chessgame;
+
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Nick
+ */
+public class Queen extends ChessPiece{
+    
+    private ChessPiece bishopQ;
+    private ChessPiece rookQ;
+
+    public Queen(Image image, String color, int boardPosition) {
+        super(image, color, boardPosition);
+
+        this.setBorder(BorderFactory.createRaisedBevelBorder());
+        this.setContentAreaFilled(false);
+        this.setBorderPainted(false);
+        this.setOpaque(false);
+        this.setSize(65, 65);
+        this.setIcon(new ImageIcon(image));
+        
+        bishopQ = new Bishop(null, null, this.boardPosition);
+        rookQ = new Rook(null, null, this.boardPosition);
+    }
+
+    @Override
+    public boolean isValidMove(int newPosition) {
+               
+        return ((bishopQ.isValidMove(newPosition)) || (rookQ.isValidMove(newPosition)));
+        
+    }
+    
+}
