a8700e99bc16b77d59d493f87bdc1d7423b79136
NickOttrando <nottrando@gmail.com>
2014-04-26 10:03:22 -0700

    began writing player class and wrote code to check if they're in check. should work but will need to test

---
+++ src/chessgame/Player.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package chessgame;
+
+/**
+ *
+ * @author Nick
+ */
+public class Player {
+    
+    String color;
+    boolean isTurn;
+    boolean isChecked;
+    
+    //make it easy to keep track of king's position on board, if we do not have
+    //each player have own array of pieces
+    int positionOfKing;
+    
+    public Player(String color){
+        this.color = color;
+    }
+    
+    public void setIsTurn(boolean turn){
+        this.isTurn = turn;
+    }
+    
+    public boolean getIsTurn(){
+        return this.isTurn;
+    }
+    
+    public boolean isChecked(){
+        
+        //pseduocode
+        //this is not as efficient as having each player have their own separate
+        //arrayLists of pieces but that may be a hassle to keep track of/update.
+        /*
+        for(int i = 0; i < ChessBoard.pieces.size; i++){
+            //these can be combined into 1 if but it's long so i broke into 2
+            if (ChessBoard.getIndex(i).getColor() != this.getColor()){        
+                if(ChessBoard.getIndex(i).isValidMove(ChessBoard.getIndex(this.positionOfKing)){
+                    this.isChecked = true;
+                    break;
+                }
+            }
+        }
+        
+        */
+        
+        return this.isChecked;
+    }
+    
+    public void setIsChecked(boolean check){
+        this.isChecked = check;
+    }
+    
+}
